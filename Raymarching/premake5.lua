5591ff30a87faabcb19a17ed967edc59a1910575
ApolloTeddy <avzaviercallen@gmail.com>
2023-01-19 14:03:49 -0500

    Refactoring

--- Raymarching/premake5.lua
+++ Raymarching/premake5.lua
@@ -1,4 +1,4 @@
-project "OpenGLApp"
+project "Raymarching"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,12 @@
 -- premake5.lua
-workspace "OpenGLApp"
+workspace "Raymarching"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "OpenGLApp"
+   startproject "Raymarching"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 group "Dependancies" 
 	include "/vendor/GLFW"
 group ""
-	include "OpenGLApp"
+	include "Raymarching"
